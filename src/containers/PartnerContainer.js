596f25d0df01f4134a1a353b4a3825da1f45b4b5
jhlim <jhlym92@gmail.com>
2020-02-10 01:49:29 +0900

    add: 상품 하단 파트너 정보 컨테이너 개발

--- src/containers/PartnerContainer.js
+++ src/containers/PartnerContainer.js
@@ -1,7 +1,46 @@
 import React from "react";
+import styled from "styled-components";
+import { useSelector } from "react-redux";
+import Row from "../components/Row";
+
+const Title = styled.span`
+  margin-right: 8px;
+  color: #868e96;
+`;
+const Content = styled.span`
+  margin-right: 16px;
+`;
 
 const PartnerContainer = () => {
-  return <div></div>;
+  const detail = useSelector(({ product: { detail } }) => detail);
+  if (!detail) return null;
+  return (
+    <>
+      <section>
+        <h3>상품정보고시 정보</h3>
+        <Row className="flex__wrap">
+          {detail.partner.product_info.map((info, index) => (
+            <span key={index}>
+              <Title>{info[0]}</Title>
+              <Content>{info[1]}</Content>
+            </span>
+          ))}
+        </Row>
+      </section>
+      {/* 업체 정보 */}
+      <section>
+        <h3>업체 정보</h3>
+        <Row className="flex__wrap">
+          {detail.partner.partner_info.map((info, index) => (
+            <span key={index}>
+              <Title>{info[0]}</Title>
+              <Content>{info[1]}</Content>
+            </span>
+          ))}
+        </Row>
+      </section>
+    </>
+  );
 };
 
 export default PartnerContainer;
