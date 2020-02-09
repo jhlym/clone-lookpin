9f9ebc4c5a065cacba0869be60f3e91fcc65dc9b
jhlim <jhlym92@gmail.com>
2020-02-09 21:30:03 +0900

    add: 라벨 컴포넌트 개발

---
+++ src/components/Label/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const Span = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${props => props.theme.red};
+  border: 2px solid ${props => props.theme.red};
+  font-weight: 600;
+  padding: 0 20px;
+  margin-left: 10px;
+`;
+
+const Label = ({ text }) => {
+  return <Span>{text}</Span>;
+};
+
+export default Label;
