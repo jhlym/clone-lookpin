252f5ec320f28cc8122e98337bb95f6104acf1b0
jhlym <ljh.github@gmail.com>
2020-02-10 08:46:05 +0900

    Updates

---
+++ precache-manifest.f5f298e5a744d7ff5ac3127b917c41ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "004d8aacac119a2acb9bbe7ab7890d88",
+    "url": "/clone-lookpin/index.html"
+  },
+  {
+    "revision": "ef98c35cc75cac6b2f68",
+    "url": "/clone-lookpin/static/css/main.565a705d.chunk.css"
+  },
+  {
+    "revision": "8ccf0ae06ba96a0cba60",
+    "url": "/clone-lookpin/static/js/2.cf72c5aa.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/clone-lookpin/static/js/2.cf72c5aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef98c35cc75cac6b2f68",
+    "url": "/clone-lookpin/static/js/main.e121138f.chunk.js"
+  },
+  {
+    "revision": "c0900f316334f417b0e7",
+    "url": "/clone-lookpin/static/js/runtime-main.3f2cc24e.js"
+  }
+]);
