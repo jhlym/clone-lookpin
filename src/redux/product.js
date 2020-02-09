20e8aa50f6d2ef7352eb73e782761ee987b8d218
jhlim <jhlym92@gmail.com>
2020-02-10 01:48:55 +0900

    add: option price 값 처리 위한 데이터 가공

--- src/redux/product.js
+++ src/redux/product.js
@@ -2,9 +2,19 @@
 import data from "../response";
 import { createAction, handleActions } from "redux-actions";
 
+// FIXME: 데이터 가공 위한 구문
+const t = {};
+data.options.data.forEach(data => {
+  const key = data.positions.join("");
+  t[key] = data.data;
+});
+data.options.case = t;
+
 const GET_PRODUCT_DETAIL = "product/GET_PRODUCT_DETAIL";
+const RESET_PRODUCT_DETAIL = "product/RESET_PRODUCT_DETAIL";
 
 export const getProductDetail = createAction(GET_PRODUCT_DETAIL);
+export const resetProductDetail = createAction(RESET_PRODUCT_DETAIL);
 
 const initialState = {
   detail: null
@@ -13,7 +23,12 @@ const initialState = {
 export default handleActions(
   {
     [GET_PRODUCT_DETAIL]: (state, payload) => ({
+      ...state,
       detail: data
+    }),
+    [RESET_PRODUCT_DETAIL]: (state, payload) => ({
+      ...state,
+      detail: null
     })
   },
   initialState
